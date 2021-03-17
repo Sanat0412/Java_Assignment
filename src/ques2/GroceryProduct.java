64fb0154b9d738724a7a7bd4aacb2e69b0464682
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-17 19:07:04 +0530

    create GroceryProduct.java

---
+++ src/ques2/GroceryProduct.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 17-Mar-21
+ *   Time: 7:06 PM
+ *   File: GroceryProduct.java
+ */
+
+package ques2;
+
+public class GroceryProduct {
+}
+
