4b4715bdf63a8e56785f9d52f4e5dc787d22c100
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-17 15:31:32 +0530

    add code to print pattern

--- src/ques1/PatternPrinting2.java
+++ src/ques1/PatternPrinting2.java
@@ -7,6 +7,28 @@
 
 package ques1;
 
+import java.util.Scanner;
+
 public class PatternPrinting2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int numberOfLines= sc.nextInt();
+        printPattern(numberOfLines);
+    }
+
+    private static void printPattern(int numberOfLines) {
+        char first ='A';
+        for (int i = 1; i <=numberOfLines; i++) {
+            for (int i1 = 0; i1 < numberOfLines - i; i1++) {
+                System.out.print(" ");
+            }
+            for (int i1 = 0; i1 < i; i1++) {
+                System.out.print(first);
+                first++;
+            }
+            System.out.println();
+        }
+    }
+
 }
 
