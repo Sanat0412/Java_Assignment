459a588534986954d5f024a3ab4925dd9ee4e6b8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-17 19:15:28 +0530

    add constructor and addItem method

--- src/ques2/GroceryProduct.java
+++ src/ques2/GroceryProduct.java
@@ -7,6 +7,32 @@
 
 package ques2;
 
+import java.util.ArrayList;
+
 public class GroceryProduct {
+    public String name;
+    public int price;
+    public int quantity;
+
+    public GroceryProduct(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        addItemInlist(this.name,this.price,this.quantity);
+    }
+
+    public void addItemInlist(String name, int price, int quantity) {
+        ArrayList  groceryList= new ArrayList();
+        for (Object o : groceryList) {
+            if(((GroceryProduct) o).name.equalsIgnoreCase(name)){
+                ((GroceryProduct) o).price=((GroceryProduct) o).price+price;
+                ((GroceryProduct) o).quantity=((GroceryProduct) o).quantity+quantity;
+            }
+            else{
+                groceryList.add(new GroceryProduct(name,price,quantity));
+            }
+        }
+    }
+
 }
 
