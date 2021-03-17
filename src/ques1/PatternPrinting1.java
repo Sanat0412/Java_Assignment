dbc35e2d61f1d212ddc88e0938f4074378f58577
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-17 15:03:42 +0530

    create PatternPrinting1.java

---
+++ src/ques1/PatternPrinting1.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 17-Mar-21
+ *   Time: 3:03 PM
+ *   File: PatternPrinting1.java
+ */
+
+package ques1;
+
+public class PatternPrinting1 {
+}
+
