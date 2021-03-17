bd807940dca6bad1cca370f8b12bc7d7765b2ef0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-17 15:10:16 +0530

    add a void method to print pattern

--- src/ques1/PatternPrinting1.java
+++ src/ques1/PatternPrinting1.java
@@ -7,6 +7,26 @@
 
 package ques1;
 
+import java.util.Scanner;
+
 public class PatternPrinting1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of lines :- ");
+        int numberOfLines= sc.nextInt();
+        printPattern(numberOfLines);
+    }
+    private static void printPattern(int numberOfLines) {
+        for (int i = 1; i <=numberOfLines; i++) {
+            for (int i1 = 0; i1 < numberOfLines - i; i1++) {
+                System.out.print(" ");
+            }
+            for (int i1 = 0; i1 < i; i1++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
 }
 
