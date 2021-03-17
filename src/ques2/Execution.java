361ed79b2cf93c663ee05c513e10f97828cf1998
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-17 19:21:42 +0530

    create Execution.java

---
+++ src/ques2/Execution.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 17-Mar-21
+ *   Time: 7:21 PM
+ *   File: Execution.java
+ */
+
+package ques2;
+
+public class Execution {
+}
+
